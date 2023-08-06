abf0173974929896ac51bfd453dcf0cf00ce62b1
Sidorov Oleg <ya.sidorovoleg8700@yandex.ru>
2023-08-06 22:19:09 +0300

    3d commit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,7 +9,7 @@ function Card({ card, onCardClick }) {
                 <h2 className="element__title">{card.name}</h2>
                 <div className="element__like-container">
                 <button className="element__like" type="button" />
-                <span className="element__counter" />
+                <span className="element__counter">{card.likes.length}</span>
                 </div>
             </div>
         </article>
